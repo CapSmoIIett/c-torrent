2aa7160e3987b178922171cf8dd6857d9b662eb5
m.susha <m.susha@va.rb>
2024-06-07 15:02:22 +0300

    Add comparing of arrays. And fix workflow.

--- tests/bencode_test.cpp
+++ tests/bencode_test.cpp
@@ -1,25 +1,22 @@
 
 #include <gtest/gtest.h>  
 
-//#include "helper.h"
+#include "helper.h"
 #include "../c-torrent/bencoder.h"
 
 TEST(BENCODE_DECODE_TEST, TestDecodeString) 
 {
     auto res = Bencoder::decode("5:hello");
-    /*
-    auto compareArray = [&](std::vector<std::string> l, std::vector<std::string> r){
-        EXPECT_EQ(l.size(), r.size());
 
-        for (int i = 0; i < l.size(); ++i) {
-            EXPECT_EQ(l[i], r[i]) << "Arrays is different at index " 
-                << i ;//<< ". "  << "l[i]: " << l[i] << ", r[i]: " << r[i]; 
-        }
-    };
+    COMPARE_ARRAY_EQ({"hello"}, Bencoder::decode("5:hello"));
+    COMPARE_ARRAY_EQ({"hell"},  Bencoder::decode("4:hello"));
+    COMPARE_ARRAY_NE({"hello"}, Bencoder::decode("4:hello"));
+    COMPARE_ARRAY_EQ({"Hello World!"}, Bencoder::decode("12:Hello World!"));
+    COMPARE_ARRAY_EQ({"~`'\"!@#$%^&*()"}, Bencoder::decode("14:~`'\"!@#$%^&*()"));
+    COMPARE_ARRAY_NE({"~`'\"!@#$%^&*()"}, Bencoder::decode("11:~`'\"!@#$%^&*()"));
+}
 
-    //COMPARE_ARRAY({"hello"}, Bencoder::decode("5:hello"));
-    compareArray({"hello"}, Bencoder::decode("5:hello"));
-    */
-
+TEST(BENCODE_DECODE_TEST, TestDecodeNumbers) 
+{
     EXPECT_TRUE(true);
 }
--- tests/helper.h
+++ tests/helper.h
@@ -2,12 +2,30 @@
 #include <gtest/gtest.h>  
 
 template<class T>
-void COMPARE_ARRAY(std::vector<T> l, std::vector<T> r)
+void COMPARE_ARRAY_EQ(std::vector<T> l, std::vector<T> r)
 {
     EXPECT_EQ(l.size(), r.size());
 
+    if (l.size() != r.size())
+        return;
+
     for (int i = 0; i < l.size(); ++i) {
         EXPECT_EQ(l[i], r[i]) << "Arrays is different at index " 
+            << i << ". "  << "l[i]: " << l[i] << ", r[i]: " << r[i]; 
+    }
+}
+
+template<class T>
+void COMPARE_ARRAY_NE(std::vector<T> l, std::vector<T> r)
+{
+    if (l.size() != r.size())
+    {
+        EXPECT_NE(l.size(), r.size());
+        return;
+    }
+
+    for (int i = 0; i < l.size(); ++i) {
+        EXPECT_NE(l[i], r[i]) << "Arrays is different at index " 
             << i << ". "  << "l[i]: " << l[i] << ", r[i]: " << r[i]; 
     }
 }
