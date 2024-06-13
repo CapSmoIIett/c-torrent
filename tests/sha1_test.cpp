d55f9eac916eb2482592d2171bd69ae78ded8b0c
m.susha <vikdrovosek@gmail.com>
2024-06-13 13:02:42 +0300

    Spizdil sha1 algo.

--- tests/sha1_test.cpp
+++ tests/sha1_test.cpp
@@ -5,9 +5,16 @@
 
 TEST(SHA1_TESTS, sha1_Test) 
 {
-    EXPECT_EQ("a9993e364706816aba3e25717850c26c9cd0d89d", SHA1::algo("abc"));
-    EXPECT_EQ("d9f0509fb7e8bd7d4c4b627dfec70c0c0e01fb34", SHA1::algo("cba"));
-    EXPECT_EQ("b1b3773a05c0ed0176787a4f1574ff0075f7521e", SHA1::algo("qwerty"));
+    SHA1 checksum;
+
+    checksum.update("abc");
+    EXPECT_EQ("a9993e364706816aba3e25717850c26c9cd0d89d", checksum.final());
+
+    checksum.update("cba");
+    EXPECT_EQ("d9f0509fb7e8bd7d4c4b627dfec70c0c0e01fb34", checksum.final());
+
+    checksum.update("qwerty");
+    EXPECT_EQ("b1b3773a05c0ed0176787a4f1574ff0075f7521e", checksum.final());
 }
 
 
