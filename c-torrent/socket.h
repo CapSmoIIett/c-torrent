9838a65a68489f3fbfffedf2c551abe351f94b83
m.susha <vikdrovosek@gmail.com>
2024-07-08 17:07:57 +0300

    Remake sockets.

--- c-torrent/socket.h
+++ c-torrent/socket.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 
@@ -37,6 +38,8 @@
 #include <memory>
 #include <iostream>
 
+namespace msock
+{
 
 class Socket final 
 {
@@ -78,16 +81,8 @@ public:
 
 private:
 #if defined(OS_WINDOWS)
-
-    struct wsadata_deleter {
-				void operator()(WSADATA* other) {
-					WSACleanup();
-					delete other;
-				}
-			};
-
     SOCKET _socket = INVALID_SOCKET;
-    std::unique_ptr<WSADATA, wsadata_deleter> _wsaData = {new WSADATA, wsadata_deleter()};
+    WSADATA _wsaData = { 0 };
 #else
     int _socket = -1;
 
@@ -96,165 +91,4 @@ private:
 };
 
 
-Socket::Socket()
-{
-#if defined(OS_WINDOWS)
-
-    if ( 0 != WSAStartup(MAKEWORD(2, 2), _wsaData.get()))
-    {
-        std::cout << "WSAStartup error " << GetLastError() << "\n";
-    }
-
-    //if ( 0 != )
-
-#endif
-
 }
-
-
-void Socket::socket()
-{
-    _socket = ::socket(AF_INET, SOCK_STREAM, 0);
-
-    if (_socket == INVALID_SOCKET) 
-    {
-        std::cout << "socket error ";
-
-        #if defined(OS_WINDOWS)
-            std::cout << GetLastError();
-        #endif
-
-        std::cout << "\n";
-    }
-}
-
-
-void Socket::connect(sockaddr_in addr)
-{
-    int res = 0;
-
-#if defined(OS_WINDOWS)
-    res = ::connect(_socket, (SOCKADDR*) &addr, sizeof(addr));
-#else
-    res = ::connect(_socket, (const struct sockaddr *) &addr, sizeof(addr));
-#endif
-    if(S_ERROR == res)
-    {
-        std::cout << "connect error " << GetLastError() << "\n";
-    }
-}
-
-
-
-void Socket::send(std::string msg)
-{
-    int res = 0;
-
-#if defined(OS_WINDOWS)
-    res = ::send(_socket, msg.c_str(), msg.size(), 0);
-#else
-    res = ::write(_socket, msg.c_str(), msg.size());
-#endif
-
-    if (S_ERROR == res)
-    {
-        std::cout << "send error " << GetLastError() << "\n";
-    }
-}
-
-void Socket::write(std::string msg)
-{
-    this->send(msg);
-}
-
-std::string Socket::recv()
-{
-    char buffer[1024];
-    int res = 0;
-
-#if defined(OS_WINDOWS)
-    res = ::recv(_socket, buffer, sizeof(buffer), 0);
-#else
-    res = ::read(_socket, buffer, sizeof(buffer));
-#endif
-
-    if (res == 0)
-    {
-        std::cout << "closed " << GetLastError() << "\n";
-        // closed
-    }
-    else if (res < 0)
-    {
-        std::cout << "error " << GetLastError() << "\n";
-        // error
-    }
-
-    std::string response(buffer, res);
-
-    return response;
-}
-
-std::string Socket::read()
-{
-    return this->recv();
-}
-
-
-void Socket::closesocket()
-{
-    int res = 0;
-
-#if defined(OS_WINDOWS)
-    res = ::closesocket(_socket);
-#else
-    res = ::close(_socket);
-#endif
-
-    if (S_ERROR == res)
-    {
-        std::cout << "closesocket error" << GetLastError() << "\n";
-    }
-}
-
-void Socket::close()
-{
-    this->closesocket();
-}
-
-std::string Socket::GetLastError()
-{
-    std::string answer;
-
-#if defined(OS_WINDOWS)
-    answer = WSAGetLastError();
-#else
-    answer = " ";
-#endif
-
-    return answer;
-}
-
-
-
-Socket::~Socket()
-{
-#if defined(OS_WINDOWS)
-    WSACleanup();
-#endif
-}
-
-
-#if defined(OS_WINDOWS)
-    unsigned long Socket::inet_addr(const char *ip)
-    {
-        return ::inet_addr(ip);
-    }
-#else
-    in_addr_t Socket::inet_addr(const char *ip)
-    {
-
-        struct hostent* host = gethostbyname(ip);
-        return ::inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
-    }
-#endif
-
