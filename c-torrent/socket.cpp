e3f67d58e247c6621fefafebb64b24c9e233bde9
m.susha <vikdrovosek@gmail.com>
2024-07-09 08:38:00 +0300

    Remake GetLastError.

--- c-torrent/socket.cpp
+++ c-torrent/socket.cpp
@@ -161,7 +161,25 @@ std::string msock::Socket::GetLastError()
     std::string answer;
 
 #if defined(OS_WINDOWS)
-    answer = WSAGetLastError();
+    DWORD err_id = ::GetLastError();
+
+    //No error message has been recorded
+    if(err_id == 0) 
+        return std::string(); 
+    
+    LPSTR messageBuffer = nullptr;
+
+    //Ask Win32 to give us the string version of that message ID.
+    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL, err_id, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+    
+    //Copy the error message into a std::string.
+    answer = std::string(messageBuffer, size);
+    
+    //Free the Win32's string's buffer.
+    LocalFree(messageBuffer);
+
 #else
     answer = " ";
 #endif
