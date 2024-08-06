e5e71510aa3b428b8f2c98dbeb187ab7eb1deee2
m.susha <vikdrovosek@gmail.com>
2024-08-06 11:31:40 +0300

    Write writing in file. :) .

--- c-torrent/async_writer.h
+++ c-torrent/async_writer.h
@@ -3,20 +3,21 @@
 #include <fstream>
 #include <mutex>
 #include <string>
+#include <fstream>
 
 class AsyncWriter
 {
 public:
-    AsyncWriter(std::string file_name) :
-        name(file_name)
-    {}
+    AsyncWriter(std::string file_name);
+    ~AsyncWriter();
 
-    void write(const std::string& str, size_t pos);
+    void write(const std::string& str, size_t pos, size_t size = -1);
     void open();
     void close();
 
 private:
     std::mutex mtx;
     std::string name;
+    std::ofstream file;
 
 };
