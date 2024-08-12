83c69c668afcc03cc50caf15f9797c0540c72a7a
m.susha <vikdrovosek@gmail.com>
2024-08-12 15:22:38 +0300

    remale logger.

--- c-torrent/logger.h
+++ c-torrent/logger.h
@@ -53,11 +53,7 @@ class Logger final
 {
 public:
     // singleton of Scott Meyers
-    static Logger* instance() 
-    {
-        static Logger inst;
-        return &inst;
-    } 
+    static Logger* instance();
 
     int deep_counter = 0;
 
@@ -65,22 +61,62 @@ public:
     friend Logger& operator<< (Logger& logger, T t);
 
 private:
-    Logger()
-    {
-    #ifdef LOG_FILE
-        file.open(LOG_FILE, std::ios::app);
-    #else
-        file.open(DEFAULT_LOG_FILE, std::ios::app);
-    #endif
-    }
-    ~Logger()
-    {
-        file.close();
-    }
+    Logger();
+    ~Logger();
 
     std::ofstream file;
-
 };
+
+
+class _Logs 
+{
+protected:
+    template <class T>
+    void write_stack_depth(T& t, int deep);
+
+    std::string get_time();
+};
+
+
+class FunctionLog final : _Logs
+{
+public:
+    FunctionLog(const char* name, const char* file, int line);
+    ~FunctionLog();
+
+private:
+    std::string function_name;
+    std::string file_name;
+    std::string row;
+};
+
+
+class __Log final : _Logs
+{
+public:
+    __Log(char type, const char* file, int line);
+    ~__Log();
+
+    template <class T>
+    friend const __Log& operator<< (const __Log& logger, T t);
+};
+
+
+template <class T>
+void mlog::_Logs::write_stack_depth(T& t, int deep)
+{
+    for (int i = 0; i < deep; ++i)
+        t << "| "; 
+}
+
+template <class T>
+const __Log& operator<< (const __Log& log, T t)
+{
+    auto logger = Logger::instance();
+    (*logger) << t;
+
+    return log;
+} 
 
 template <class T>
 Logger& operator<< (Logger& logger, T t)
@@ -90,111 +126,14 @@ Logger& operator<< (Logger& logger, T t)
     return logger;
 } 
 
-class _Logs 
-{
-protected:
-    template <class T>
-    void write_stack_depth(T& t, int deep)
-    {
-        for (int i = 0; i < deep; ++i)
-            t << "| "; 
-    }
-
-    std::string get_time()
-    {
-        char buffer[26];
-        std::tm tm;
-        time_t time = std::time(nullptr);
-
-    #if defined(OS_WINDOWS)
-        gmtime_s(&tm, &time);
-    #else
-        gmtime_r(&time, &tm);
-    #endif
-
-        strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", &tm);
-
-        return std::string(buffer);
-    }
-
-};
-
-
-
-class FunctionLog final : _Logs
-{
-public:
-    FunctionLog(const char* name, const char* file, int line) :
-        function_name(name), file_name(file), row(std::to_string(line))
-    {
-        auto logger = Logger::instance();
-
-        (*logger) << "[" << get_time() << "] ";
-        write_stack_depth(*logger, logger->deep_counter);
-
-        // enter in function
-        (*logger) << "|_ [" << file << ":" << line << "] ["<< function_name << "]" << "\n"; 
-
-        ++logger->deep_counter;
-    }
-
-    ~FunctionLog()
-    {
-        auto logger = Logger::instance();
-        --logger->deep_counter;
-
-        (*logger) << "[" << get_time() << "] ";
-        write_stack_depth(*logger, logger->deep_counter);
-
-        // exit from function
-        (*logger) << "|_| [" << file_name << ":" << std::string(row.size(), '_') << "] ";
-        (*logger) << "[" << function_name << "]" << "\n"; 
-
-    }
-
-private:
-    std::string function_name;
-    std::string file_name;
-    std::string row;
-};
-
-class Log final : _Logs
-{
-public:
-    Log(char type, const char* file, int line)
-    {
-        auto logger = Logger::instance();
-        (*logger) << "[" << get_time() << "] ";
-        write_stack_depth(*logger, logger->deep_counter);
-        (*logger) << "| [" << file << ":" << line << "] [" << type << "] : "; 
-    }
-
-    ~Log()
-    {
-        auto logger = Logger::instance();
-        (*logger) << "\n";
-    }
-
-    template <class T>
-    Logger& operator<< (T t)
-    {
-        auto logger = Logger::instance();
-        (*logger) << t;
-
-        return *logger;
-    } 
-};
-
-inline Log create_log(char type, const char* file, const int line)
-{
-    return Log(type, file, line);
-}
-
 }
 
 #define LOG mlog::FunctionLog __function_log__(__FUNCTION__, __FILE__, __LINE__);
 
-#define __log(ch) mlog::create_log(ch,  __FILE__, __LINE__)
+//#define __log(ch) mlog::__create_log(ch, __FILE__, __LINE__)
+//#define __log() mlog::Log('i', __FILE__, __LINE__)
+#define _log(ch) mlog::__Log(ch, __FILE__, __LINE__)
+
 //#define log() make_log(__FILE__, __LINE__)
 
 
